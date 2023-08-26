a86cae23fc54848e9ff28cdec4e7a4ae7d3d6240
Yane <yanelobo@gmail.com>
2023-08-26 14:57:32 -0300

    aula

--- exercicios/para-sala/index.js
+++ exercicios/para-sala/index.js
@@ -1,6 +1,43 @@
 // arquivo para testes do exercicio exercicio-em-aula.md
 
 // 1.
+class Product {
+
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+
+    showDetails() {
+        console.log(`Nome: ${this.name} \nPreço: ${this.price}`);
+    }
+
+}
+
+class Cart {
+    constructor() {
+        this.products = [];
+    }
+
+    calculateTotalValue(unitPrice, quantity) {
+        return unitPrice * quantity
+    }
+
+    addToCart(product, quantity) {
+        this.products.push({product, quantity})
+        console.log(`Adiciono ${quantity} do produto ${product.name} ao carrinho. \n
+        Preço total: ${this.calculateTotalValue(product.price, quantity)}`);
+    }
+}
+
+const AlbumDoMamamoo = new Product("Álbum do Mamamoo", 200)
+AlbumDoMamamoo.showDetails()
+const novaCompra = new Cart()
+novaCompra.addToCart(AlbumDoMamamoo, 1)
+
+
+
+
 // 2.
 // 3.
 // 4.

c15bb3c4bf534029a5e962af46871b4375fb03f8
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-09-01 23:08:08 -0300

    refactor: altera sintaxe para jest

--- exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system-tests/alarm.test.js
+++ exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system-tests/alarm.test.js
@@ -1,16 +1,14 @@
 /* globals describe, it */
-var chai = require('chai');
-chai.should();
 
 var Alarm = require('../tire-pressure-monitoring-system/alarm.js');
 
-describe('Tyre Pressure Monitoring System', function() {
+describe("Tyre Pressure Monitoring System", () => {
 
-	describe('Alarm', function() {
+	describe("Alarm", () => {
 
-		it('foo', function() {
+		it("should return", () => {
 			var alarm = new Alarm();
-			alarm.alarmOn().should.equal(false);
+			expect(alarm.alarmOn()).toBe(false);
 		});
 
 	});

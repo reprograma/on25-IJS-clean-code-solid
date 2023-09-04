ec9d1446ba77b88a723ddb25a30100e11763a427
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-09-03 21:56:29 -0300

    feat:adiciona classe Sensor

---
+++ exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system/SensorClass.js
@@ -0,0 +1,18 @@
+class Sensor {
+  Offset() {
+    return 16;
+  }
+
+  samplePressure() {
+    let pressureTelemetryValue = Math.floor(6 * Math.random() * Math.random());
+    return pressureTelemetryValue;
+  }
+
+  popNextPressurePsiValue() {
+    let pressureTelemetryValue = Sensor.samplePressure();
+
+    return this.Offset() + pressureTelemetryValue;
+  }
+}
+
+module.exports = { Sensor };

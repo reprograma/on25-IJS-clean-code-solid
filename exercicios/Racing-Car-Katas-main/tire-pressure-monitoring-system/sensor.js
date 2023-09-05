1db9a406dc28787127f6d1e5a6cd858a94659117
Katiene Souza <katienestudy@gmail.com>
2023-09-04 21:43:31 -0300

    add refatoração

--- exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system/sensor.js
+++ exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system/sensor.js
@@ -1,24 +1,18 @@
-// The reading of the pressure value from the sensor is simulated in this implementation.
-// Because the focus of the exercise is on the other class.
+export class Sensor {
 
-Sensor = function() {
+	 offset() {
+		return 16;
+	 }
 
-	Sensor.Offset = function() { return 16; };
-
-	Sensor.samplePressure = function() {
-		// placeholder implementation that simulate a real sensor in a real tire
-		var pressureTelemetryValue = Math.floor(6 * Math.random() * Math.random());
+	 samplePressure() {
+		let pressureTelemetryValue = Math.floor(6 * Math.random() * Math.random());
 		return pressureTelemetryValue;
-	};
-};
+	 }
 
-Sensor.prototype = {
+	 popNextPressurePsiValue() {
+		let pressureTelemetryValue = this.samplePressure();
 
-	popNextPressurePsiValue: function() {
-		var pressureTelemetryValue = Sensor.samplePressure();
+		return this.offset() + pressureTelemetryValue;
+	 }
+}
 
-		return Sensor.Offset() + pressureTelemetryValue;
-	}
-};
-
-module.exports = Sensor;

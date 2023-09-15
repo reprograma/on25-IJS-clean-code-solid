5207c43e08d39402f57b1ecb35d023d33fd0cabc
Deborah Gomes Souza <deborah270691@gmail.com>
2023-09-14 21:32:15 -0300

    Trabalho SOLID

--- exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system-tests/alarm-test.js
+++ exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system-tests/alarm-test.js
@@ -1,18 +1,44 @@
-/* globals describe, it */
-var chai = require('chai');
-chai.should();
+const { expect } = require('chai');
+const { Alarm } = require('./alarm.js');
 
-var Alarm = require('../tire-pressure-monitoring-system/alarm.js');
 
-describe('Tyre Pressure Monitoring System', function() {
+class MockSensor {
+    popNextPressurePsiValue() {
+        
+        return 25;
+    }
+}
 
-	describe('Alarm', function() {
+describe('Alarm', () => {
+    it('should not be initially on', () => {
+        const alarm = new Alarm(new MockSensor());
+        expect(alarm.alarmOn()).to.equal(false);
+    });
 
-		it('foo', function() {
-			var alarm = new Alarm();
-			alarm.alarmOn().should.equal(false);
-		});
+    it('should turn on when pressure is too low', () => {
+        const sensor = new MockSensor();
+        sensor.popNextPressurePsiValue = () => 15; 
+        const alarm = new Alarm(sensor);
 
-	});
+        alarm.check();
+        expect(alarm.alarmOn()).to.equal(true);
+    });
 
+    it('should turn on when pressure is too high', () => {
+        const sensor = new MockSensor();
+        sensor.popNextPressurePsiValue = () => 22; 
+        const alarm = new Alarm(sensor);
+
+        alarm.check();
+        expect(alarm.alarmOn()).to.equal(true);
+    });
+
+    it('should stay off when pressure is within the normal range', () => {
+        const sensor = new MockSensor();
+        sensor.popNextPressurePsiValue = () => 18; 
+        const alarm = new Alarm(sensor);
+
+        alarm.check();
+        expect(alarm.alarmOn()).to.equal(false);
+    });
 });
--- exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system/sensor.js
+++ exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system/sensor.js
@@ -1,6 +1,3 @@
-// The reading of the pressure value from the sensor is simulated in this implementation.
-// Because the focus of the exercise is on the other class.
-
 Sensor = function() {
 
 	Sensor.Offset = function() { return 16; };
